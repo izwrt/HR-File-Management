ac455a340b261e6d72538d03494a9d9fc04981d2
Jeevitha G S <134584018+JeevithaPoojary@users.noreply.github.com>
2024-08-27 15:07:50 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom/client";
+import SetPassword from "./components/auth/SetPassword";
 import Login from "./components/auth/Login";
 
-const root = ReactDom.createRoot(document.getElementById('root'));
-root.render(<Login/>);
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(<Login />);
