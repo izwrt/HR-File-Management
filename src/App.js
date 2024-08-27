1a7d0c5cdd271f0f12616b0974b842947a7e5d78
izwrt <ishwart466@gmail.com>
2024-08-27 14:35:17 +0530

    error solved

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
-<<<<<<< HEAD
-
-const HelloWorld = () => {
-  return <h1>Hello World.</h1>;
-};
-
-const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(<HelloWorld />);
-=======
 import Login from "./components/auth/Login";
 
 const root = ReactDom.createRoot(document.getElementById('root'));
 root.render(<Login/>);
->>>>>>> origin/master
