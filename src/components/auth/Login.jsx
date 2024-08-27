ac2cea7ad40a360ff9591b6106fb71f61f4616ea
Jeevitha G S <134584018+JeevithaPoojary@users.noreply.github.com>
2024-08-27 15:07:50 +0530

    test

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,22 +1,19 @@
-import TextBox from "../common/TextBox"
+import TextBox from "../common/TextBox";
 
 const Login = () => {
-    return (
-            <div className = "bg-neutral-500  w-screen h-screen flex justify-center">
-                <div className ="bg-red-300 w-2/5 h-auto flex items-center flex-col p-10 ">
-                <h1 className="custom-font m-[20%]">Login</h1>
-                    <div className="bg-green-400 p-4 flex justify-center flex-col gap-10">
-                        <TextBox placeholder={"UserName"} type={"text"}/>
-                        <TextBox placeholder={"password"} type={"password"}/>
-                    </div>
+  return (
+    <div className="bg-neutral-500  w-screen h-screen flex justify-center">
+      <div className="bg-red-300 w-2/5 h-auto flex items-center flex-col p-10 ">
+        <h1 className="custom-font m-[20%]">Login</h1>
+        <div className="bg-green-400 p-4 flex justify-center flex-col gap-10">
+          <TextBox placeholder={"UserName"} type={"text"} />
+          <TextBox placeholder={"password"} type={"password"} />
+        </div>
 
-                    <div>
-                        {/**button and forgot pass */}
-                    </div>
-                </div>
-                
-            </div>
-            )
-}
+        <div>{/**button and forgot pass */}</div>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
