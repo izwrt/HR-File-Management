a58e1244f907092fcf461d9f50deeaa802725246
izwrt <ishwart466@gmail.com>
2024-08-27 22:54:24 +0530

    login phase-2

--- src/components/common/LoginTextBox.jsx
+++ src/components/common/LoginTextBox.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 
-export default function LoginTextBox({placeholder,type,isTrue}) {
+export default function LoginTextBox({ placeholder, type, isTrue, dispatch, value,name }) {
   console.log(isTrue);
+
+  function handleChange(e) {
+    dispatch({
+      type: name,
+      payload: e.target.value,
+    });
+  }
+
   return (
     <input
-  className={`rounded-2xl font-medium placeholder:font-normal h-14 px-4 focus:outline-none text-2xl bg-transparent border-[1px] ${
-    isTrue ? 'border-gray-400' : 'border-red-400'
-  }`}
-  placeholder={placeholder}
-  type={type}
-/>
-
-  )
+      className={`rounded-2xl font-normal placeholder:font-normal placeholder:text-lg px-4 p-3            
+                focus:outline-customBlue text-xl bg-transparent border-[1px] text-gray-600 ${
+                  isTrue ? 'border-gray-400' : 'border-red-400' }`}
+      placeholder={placeholder}
+      type={type}
+      onChange={handleChange}
+      value={value}
+    />
+  );
 }
