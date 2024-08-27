9a00a664a4ebc38d74312851a148bdaa3cafe961
Krithi382 <krithiprabhu94@gmail.com>
2024-08-27 18:08:46 +0530

    Login Page Phase1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        customBlue: '#3D91A3',
+      }
+    },
   },
   plugins: [],
 }
