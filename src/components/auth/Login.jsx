7a46ce7eac2b097f2d19343917de37cbc8a406c9
izwrt <ishwart466@gmail.com>
2024-08-28 10:43:05 +0530

    login css

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,4 +1,6 @@
-import TextBox from "../common/TextBox";
+import LoginTextBox from "../common/LoginTextBox";
+import LoginButton from "../common/LoginButton";
+import { useReducer, useState } from "react";
 
 const Login = () => {
   const [isTrue, setIsTrue] = useState(true);
@@ -11,7 +13,8 @@ const Login = () => {
   return (
     <div className="  w-screen h-screen flex justify-center">
       <form className=" w-2/5 h-auto flex items-center flex-col p-10 px-20">
-        <h1 className="custom-font text-6xl font-bold m-[16%]">Login</h1>
+        <h1 className="custom-font text-5xl font-bold m-[20%]">Login</h1>
+
         <div className="w-full p-4 flex justify-center flex-col gap-8">
           <LoginTextBox isTrue={isTrue} placeholder={"Email"} type={"text"} />
           <LoginTextBox
@@ -20,9 +23,10 @@ const Login = () => {
             type={"password"}
           />
         </div>
-        <div className="mt-8 p-4 w-full flex justify-center items-center flex-col gap-12">
-          <LoginButton>Login</LoginButton>
-          <a className="text-customBlue font-semibold text-xl" href="">
+
+        <div className="mt-4 p-4 w-full flex justify-center items-center flex-col gap-8">
+          <LoginButton dispatch={dispatch}>Login</LoginButton>
+          <a className="text-customBlue font-semibold text-lg" href="">
             Forgot Password ?
           </a>
         </div>
