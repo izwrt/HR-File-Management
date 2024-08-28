7d2488f94ec8dd0092437b6854b75e361761087a
izwrt <ishwart466@gmail.com>
2024-08-28 17:22:27 +0530

    userReducer

--- src/Index.js
+++ src/Index.js
@@ -4,5 +4,18 @@ import {
     Route,
     Link,
   } from "react-router-dom";
+import Login from "./components/auth/Login";
+import SetPassword from "./components/auth/SetPassword.jsx"
 
-  
+const appRouter = createBrowserRouter(
+    [
+        {
+            path:"/login",
+            element: <Login/>
+        },
+        {
+            path:"/setpassword",
+            element: <SetPassword/>
+        }
+    ]
+)
