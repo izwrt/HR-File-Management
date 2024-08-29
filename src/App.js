d95d40b2c84b3baadb32727358aa7f012844b51d
Jeevitha G S <134584018+JeevithaPoojary@users.noreply.github.com>
2024-08-29 13:56:06 +0530

    issue solved

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 import Login from "./components/auth/Login";
+import SetPassword from "./components/auth/SetPassword";
+import { CustomReducerProvider } from "./utils/useContext/CustomReducerContext";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/setpassword",
+    element: <SetPassword />,
+  },
+]);
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
   <CustomReducerProvider>
-    <RouterProvider router={appRouter} />
+    <RouterProvider router={router} />
   </CustomReducerProvider>
 );
