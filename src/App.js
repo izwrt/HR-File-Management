7f5c1014ad832fb0c9bccbb52175115681e94d57
Krithi382 <krithiprabhu94@gmail.com>
2024-08-29 14:16:55 +0530

    reminder icon

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 import Login from "./components/auth/Login";
+import Dashboard from "./components/auth/Dashboard";
 import SetPassword from "./components/auth/SetPassword";
-import { CustomReducerProvider } from './utils/useContext/CustomReducerContext';
+import { CustomReducerProvider } from "./utils/useContext/CustomReducerContext";
 
-import {
-    createBrowserRouter,
-    RouterProvider
-} from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
-   {
-    path: "/login",
-    element: <Login/>
-   },
-   {
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+  {
     path: "/setpassword",
-    element: <SetPassword/>
-   }
-])
+    element: <SetPassword />,
+  },
+]);
 
-const root = ReactDom.createRoot(document.getElementById('root'));
+const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
-<CustomReducerProvider>
-<RouterProvider router={router}/>
-</CustomReducerProvider>
-
-
+  <CustomReducerProvider>
+    <RouterProvider router={router} />
+  </CustomReducerProvider>
 );
--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Logo from "../../assets/images/logo.jsx";
+import Setting from "../../assets/images/setting.jsx";
+import Reminder from "../../assets/images/reminder.jsx";
 
 const Header = () => {
   return (
@@ -8,7 +10,12 @@ const Header = () => {
         <div className="bg-cyan-600 p-1 w-44 h-16 flex items-center justify-center ">
           <Logo />
         </div>
-        <div className="font-semibold font-slate-500">Home</div>
+        <div className=" bg-orange-200  flex flex-row gap-4 ">
+          <div className="font-semibold font-slate-500">Home</div>
+          <div className="font-semibold font-slate-500">Dashboard</div>
+          <Setting />
+          {/* <Reminder /> */}
+        </div>
       </div>
     </>
   );
