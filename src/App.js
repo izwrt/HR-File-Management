4fc20ec26fe06e4c031c6329e544b838a40572cd
Krithi382 <krithiprabhu94@gmail.com>
2024-08-29 10:25:19 +0530

    Header Phase1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 import Login from "./components/auth/Login";
+import SetPassword from "./components/auth/SetPassword";
+import Dashboard from "./components/auth/Dashboard";
 
-const root = ReactDom.createRoot(document.getElementById('root'));
-root.render(<Login/>);
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(<Dashboard />);
