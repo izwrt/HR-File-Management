fbbbe7e2ec8c18c2a707df7f3539257aa104dd90
Nakul V Kamath <nakulkamath02@gmail.com>
2024-09-17 10:44:01 +0530

    Dashboard pages

--- src/components/common/MainDashboardCard.jsx
+++ src/components/common/MainDashboardCard.jsx
@@ -1,10 +1,10 @@
 function MainDashboardCard({ smallText, largeNumber }) {
   return (
-    <div className="w-1/5 bg-white shadow-lg rounded-lg flex items-center p-8">
-      <div className="">
-        <p className="text-black font-bold text-sm">New Joinings this Month</p>
-        <p className="text-6xl font-bold mt-5 ">0</p>
-      </div>
+    <div className="bg-white shadow-lg rounded-lg p-6 h-40 md:h-52">
+      <p className="text-black font-bold text-base md:text-lg mt-5">
+        New Joinings this Month
+      </p>
+      <p className="text-4xl md:text-6xl font-bold mt-5 ">0</p>
     </div>
   );
 }
