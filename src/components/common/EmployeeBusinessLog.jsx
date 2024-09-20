30e4ad16280cf057b558b84abdf0065d1fbdf2d0
izwrt <ishwart466@gmail.com>
2024-09-20 17:51:52 +0530

    business unit pages is implemented

--- src/components/common/EmployeeBusinessLog.jsx
+++ src/components/common/EmployeeBusinessLog.jsx
@@ -1,7 +1,8 @@
-const EmployeeBusinessLog = ({employeeName,client,startDate,endDate,status,action,color,fontWeight,position}) => {
+const EmployeeBusinessLog = ({employeeName,client,startDate,endDate,status,action,color,fontWeight,position,id}) => {
     return(
         <div>
             <div className={`text-black ${fontWeight} text-sm p-3 pr-12 custom-font-mavan-pro ${color} ${position} flex justify-between`}>
+              <span className="opacity-60 w-[100px]">{id}</span>
               <span className="opacity-60 w-[200px]">{employeeName}</span>
               <span className="opacity-60 w-[100px]">{client} </span>
               <span className="opacity-60 w-[100px]">{startDate} </span>
--- src/components/pages/BusinessUnit.jsx
+++ src/components/pages/BusinessUnit.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React,{ useState, useMemo, useEffect } from 'react';
 import { CiFilter, CiSearch } from "react-icons/ci";
+import apiFecthEmployees from '../../../api/apiFecthEmployees';
 import CountContainer from "../common/CountContainer";
 import EmployeeBusinessLog from '../common/EmployeeBusinessLog';
 
 export default function BusinessUnit() {
+  const employeeDetails = apiFecthEmployees();
   return (
     <div className="h-full mt-16 ml-[220px]">
       <div className=" p-8 pr-12 flex gap-[12%] 2xl:h-[220px] ">
@@ -16,8 +18,8 @@ export default function BusinessUnit() {
         <div className="bg-white rounded-lg px-5 py-4 flex flex-col gap-3 h-full shadow-sm border-solid border border-slate-100 shadow-gray-200  ">
           <div className=" flex justify-between items-center">
             <span className="text-black font-semibold text-base 2xl:text-base custom-font-mavan-pro opacity-80 ">Employees</span>
-            <span className="flex gap-3 items-center">
-              <label for="search" className=" border border-slate-200  flex w-[200px] items-center rounded-lg p-[2px] px-[6px] gap-1.5 textbox-color">
+            <span className="flex gap-1 items-center">
+              <label htmlFor="search" className=" border border-slate-200  flex w-[200px] items-center rounded-lg p-[2px] px-[6px] gap-1.5 textbox-color">
                 <CiSearch className="size-5 stroke-1 h-fit opacity-40"/>
                 <input type="text" name="search" className="w-full focus:outline-none textbox-color custom-font-mavan-pro opacity-80" placeholder="Search" />
               </label>
@@ -27,24 +29,36 @@ export default function BusinessUnit() {
           
           <div className="flex flex-col gap-3 h-fit overflow-auto overflow">
             <div className="sticky top-0 z-10 backdrop-blur-sm">
-              <EmployeeBusinessLog employeeName="Employee Name" client={"Client"} startDate={"Start Date"} endDate={"End Date"} status={"Status"}  action={"Action"} color="customColorBlue-lite" fontWeight={"font-semibold"}/>
-            </div>
+                <EmployeeBusinessLog
+                  employeeName="Employee Name"
+                  client={"Client"}
+                  startDate={"Start Date"}
+                  endDate={"End Date"}
+                  status={"Status"}
+                  action={"Action"}
+                  color="customColorBlue-lite"
+                  fontWeight={"font-semibold"}
+                  id={"Employee Id"}
+                />
+              </div>
 
-            <EmployeeBusinessLog employeeName={"Muthuswami Venugopal Iyer"} client={"One Inc"} startDate={"01/01/2024"} endDate={"01/01/2025"} status={"Active"} action={"Action"} color="textbox-color" fontWeight={"font-medium"}/>
-            <EmployeeBusinessLog employeeName={"Muthuswami Venugopal Iyer"} client={"One Inc"} startDate={"01/01/2024"} endDate={"01/01/2025"} status={"Active"} action={"Action"} color="textbox-color" fontWeight={"font-medium"}/>
-            <EmployeeBusinessLog employeeName={"Muthuswami Venugopal Iyer"} client={"One Inc"} startDate={"01/01/2024"} endDate={"01/01/2025"} status={"Active"} action={"Action"} color="textbox-color" fontWeight={"font-medium"}/>
-            <EmployeeBusinessLog employeeName={"Muthuswami Venugopal Iyer"} client={"One Inc"} startDate={"01/01/2024"} endDate={"01/01/2025"} status={"Active"} action={"Action"} color="textbox-color" fontWeight={"font-medium"}/>
+            {employeeDetails.map((emp) => {
+              return(
+                <EmployeeBusinessLog
+                  key={emp.id} // Use a unique key for each employee
+                  employeeName={emp.name} // Assuming these properties exist
+                  client={emp.client}
+                  startDate={emp.startDate}
+                  endDate={emp.endDate}
+                  status={emp.status}
+                  action={"Action"}
+                  color="textbox-color"
+                  fontWeight={"font-medium"}
+                  id={emp.empid}
+                />
+              )
+            })}
 
-            <EmployeeBusinessLog employeeName={"Muthuswami Venugopal Iyer"} client={"One Inc"} startDate={"01/01/2024"} endDate={"01/01/2025"} status={"Active"} action={"Action"} color="textbox-color" fontWeight={"font-medium"}/>
-            <EmployeeBusinessLog employeeName={"Muthuswami Venugopal Iyer"} client={"One Inc"} startDate={"01/01/2024"} endDate={"01/01/2025"} status={"Active"} action={"Action"} color="textbox-color" fontWeight={"font-medium"}/>
-            <EmployeeBusinessLog employeeName={"Muthuswami Venugopal Iyer"} client={"One Inc"} startDate={"01/01/2024"} endDate={"01/01/2025"} status={"Active"} action={"Action"} color="textbox-color" fontWeight={"font-medium"}/>
-            <EmployeeBusinessLog employeeName={"Muthuswami Venugopal Iyer"} client={"One Inc"} startDate={"01/01/2024"} endDate={"01/01/2025"} status={"Active"} action={"Action"} color="textbox-color" fontWeight={"font-medium"}/>
-
-            <EmployeeBusinessLog employeeName={"Muthuswami Venugopal Iyer"} client={"One Inc"} startDate={"01/01/2024"} endDate={"01/01/2025"} status={"Active"} action={"Action"} color="textbox-color" fontWeight={"font-medium"}/>
-            <EmployeeBusinessLog employeeName={"Muthuswami Venugopal Iyer"} client={"One Inc"} startDate={"01/01/2024"} endDate={"01/01/2025"} status={"Active"} action={"Action"} color="textbox-color" fontWeight={"font-medium"}/>
-            <EmployeeBusinessLog employeeName={"Muthuswami Venugopal Iyer"} client={"One Inc"} startDate={"01/01/2024"} endDate={"01/01/2025"} status={"Active"} action={"Action"} color="textbox-color" fontWeight={"font-medium"}/>
-            <EmployeeBusinessLog employeeName={"Muthuswami Venugopal Iyer"} client={"One Inc"} startDate={"01/01/2024"} endDate={"01/01/2025"} status={"Active"} action={"Action"} color="textbox-color" fontWeight={"font-medium"}/>
-            
           </div>
         </div>
       </div>
