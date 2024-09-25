acf6b7f18a303fecd43336caa0381e225fd34644
Jeevitha G S <134584018+JeevithaPoojary@users.noreply.github.com>
2024-09-25 18:19:14 +0530

    upper cards done

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,27 +1,25 @@
 import React from "react";
 import Footer from "../common/Footer";
-
+import Cards from "../common/Cards";
 
 import ReminderCard from "../common/ReminderContainer";
-
 
 const Home = () => {
   return (
     <div className="bg-red-300 w-screen mt-16">
-          <h1 className="custom-font-mavan-pro font-bold text-lg p-5 homeFontColor">
-             Important Reminders
-          </h1>
-          <ReminderCard/>
-          <h1 className="custom-font-mavan-pro font-bold text-lg p-5 homeFontColor">
-            Business Unit Status
-          </h1>
-          <h1 className="custom-font-mavan-pro font-bold text-lg p-5 homeFontColor">
-            Business Units
-          </h1>
-          <Footer />
-    </div >
-      
-
+      <Cards />
+      <h1 className="custom-font-mavan-pro font-bold text-lg p-5 homeFontColor">
+        Important Reminders
+      </h1>
+      <ReminderCard />
+      <h1 className="custom-font-mavan-pro font-bold text-lg p-5 homeFontColor">
+        Business Unit Status
+      </h1>
+      <h1 className="custom-font-mavan-pro font-bold text-lg p-5 homeFontColor">
+        Business Units
+      </h1>
+      <Footer />
+    </div>
   );
 };
 
