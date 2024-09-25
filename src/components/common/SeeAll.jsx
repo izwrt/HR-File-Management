e60e33ba1c778fac79332d846048e130db505b45
Krithi382 <krithiprabhu94@gmail.com>
2024-09-25 18:38:12 +0530

    seeall button added

---
+++ src/components/common/SeeAll.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SeeAll = () => {
+  return (
+    <div className="flex justify-end items-center w-full pr-12">
+      <h2 className="custom-font-mavan-pro font-semibold text-sm align-middle homeUnitNavSelected flex items-center p-1">
+        See all <span className="ml-2">➔</span>
+      </h2>
+    </div>
+  );
+};
+
+export default SeeAll;
