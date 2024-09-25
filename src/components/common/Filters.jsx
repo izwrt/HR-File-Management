f2a9c1de2ee87e51728b41d505b736ff530ec77d
izwrt <ishwart466@gmail.com>
2024-09-25 21:59:51 +0530

    filter page phase 2

--- src/components/common/Filters.jsx
+++ src/components/common/Filters.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Close from "../../assets/images/close.png";
 
 const clients = ['One Inc', 'Slide', 'Metaz'];
 const statuses = ['Active', 'Inactive', 'Relieved'];
@@ -25,16 +26,16 @@ const Filter = ({ onClose, onApply }) => {
     };
 
     return (
-        <div className="flex flex-col gap-4 p-7  ">
-            <div className="custom-font-mavan-pro text-lg flex justify-between font-semibold  text-gray-950 text-opacity-70">Search filters
-            <div className=" cursor-pointer" onClick={onClose}>
-                X
-            </div>
+        <div className="flex flex-col gap-4 py-4 px-6 ">
+            <div className="custom-font-mavan-pro text-lg flex items-center justify-between font-semibold  text-gray-950 text-opacity-70">Search filters
+            <button className="cursor-pointer w-5 h-fit rounded-full overflow-hidden transition-all ease-in-out  duration-300 hover:scale-90 hover:opacity-60 focus:bg-slate-200" onClick={onClose}>
+                <img src={Close} alt="" />
+            </button>
             </div>
             <div>
-            <span className="flex gap-24">
+            <span className="flex gap-20">
                 <div className="flex flex-col gap-2">
-                    <h4 className="border-b border-gray-600 custom-font-mavan-pro pb-2 mb-2 text-base">Client</h4>
+                    <h4 className="border-b border-gray-500 custom-font-mavan-pro pb-2 pr-16 mb-2 text-base">Client</h4>
                     {clients.map(client => (
                         <label key={client} className="custom-font-mavan-pro text-base opacity-80">
                             <input 
@@ -48,7 +49,7 @@ const Filter = ({ onClose, onApply }) => {
                 </div>
 
                 <div className="flex flex-col gap-2">
-                    <h4 className="border-b border-gray-600 custom-font-mavan-pro pb-2 mb-2 text-base">Status</h4>
+                    <h4 className="border-b border-gray-500 custom-font-mavan-pro pb-2 pr-16 mb-2 text-base">Status</h4>
                     {statuses.map(status => (
                         <label key={status} className="custom-font-mavan-pro text-base opacity-80 ">
                             <input 
@@ -64,9 +65,9 @@ const Filter = ({ onClose, onApply }) => {
             </span>
             
             </div>
-            <div className="text-black customColorBlue-lite h-fit rounded-lg cursor-pointer px-4 py-2 w-fit custom-font-mavan-pro self-center mt-2 text-opacity-80 hover:opacity-70 hover:scale-95 transition-all duration-100" onClick={applyFilters}>
-    Apply
-</div>
+            <button className="text-black customColorBlue-lite h-fit rounded-lg cursor-pointer px-4 py-2 w-fit custom-font-mavan-pro self-center mt-2 text-opacity-80 hover:opacity-70 hover:scale-95 transition-all duration-100" onClick={applyFilters}>
+                Apply
+            </button>
 
 
         </div>
