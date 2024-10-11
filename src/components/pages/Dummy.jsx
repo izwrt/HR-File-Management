d274f442c31d84fd548aff77bfe75dcc27119d88
Krithi382 <krithiprabhu94@gmail.com>
2024-10-11 17:30:05 +0530

    pop with navigation

--- src/components/pages/Dummy.jsx
+++ src/components/pages/Dummy.jsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import { NavLink,Outlet } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink, Outlet } from "react-router-dom";
+import PopupComponent from "./PopupComponent";
 
-const fruits =[
-    "dummyone", "Orange", "Banana", "Grapes"
-];
+const fruits = ["dummyone", "salary", "Banana", "Grapes"];
 
 const Dummy = () => {
+  const [isPopupVisible, setPopupVisible] = useState(false);
+  const [popupHeading, setPopupHeading] = useState("");
+  const [popupProps, setPopupProps] = useState([]);
+
+  const handleLinkClick = (fruit) => {
+    setPopupHeading(`${fruit}`);
+    if (fruit === "dummyone") {
+      setPopupProps(["Certifications"]);
+    } else if (fruit === "salary") {
+      setPopupProps(["Salary Structure", "Email Screenshot"]);
+    } else if (fruit === "Banana") {
+      setPopupProps(["Banana Variety", "Banana Benefits"]);
+    } else if (fruit === "Grapes") {
+      setPopupProps(["Grape Types", "Grape Uses"]);
+    }
+
+    setPopupVisible(true);
+  };
+
+  const closePopup = () => {
+    setPopupVisible(false);
+  };
+
   return (
-    <div className=' mt-16 ml-[220px] 2xl:ml-[230px] md:ml-0 flex gap-10'>
-      {fruits.map(f => <NavLink 
-                          to={f}
-                        className="fruit-link"
-                        activeClassName="active">
-                          {f}
-                        </NavLink>)}
+    <div className="mt-16 ml-[220px] 2xl:ml-[230px] md:ml-0 flex gap-10">
+      {fruits.map((f) => (
+        <NavLink
+          to={f}
+          className="fruit-link"
+          activeClassName="active"
+          onClick={() => handleLinkClick(f)}
+          key={f}
+        >
+          {f}
+        </NavLink>
+      ))}
 
-                        <Outlet/>
+      <Outlet />
+
+      {isPopupVisible && (
+        <PopupComponent
+          heading={popupHeading}
+          onClose={closePopup}
+          props={popupProps}
+        />
+      )}
     </div>
+  );
+};
 
-  )
-}
-
-export default Dummy
+export default Dummy;
