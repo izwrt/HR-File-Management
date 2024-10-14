0e4230eff563205bcc27592114d015013cfa2400
Krithi382 <krithiprabhu94@gmail.com>
2024-10-14 21:22:11 +0530

    only 2 nav link done

--- src/components/common/Salary.jsx
+++ src/components/common/Salary.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import PopupComponent from "../pages/PopupComponent";
 
 const Salary = () => {
-  return (
-    <div>
-      hii    
-      </div>
-  )
-}
+  const [isPopupVisible, setPopupVisible] = useState(true);
 
-export default Salary
+  const closePopup = () => {
+    setPopupVisible(false);
+  };
+
+  const popupProps = [
+    "Month one",
+    "Month two",
+    "Month three",
+    "Form 16",
+    "Bank Statement",
+  ];
+  return (
+    isPopupVisible && (
+      <PopupComponent
+        heading={"jgfvc"}
+        onClose={closePopup}
+        props={popupProps}
+      />
+    )
+  );
+};
+
+export default Salary;
