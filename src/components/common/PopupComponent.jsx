0d6c0fab6144585d884cddb7db530d4326fcf91a
izwrt <ishwart466@gmail.com>
2024-10-15 21:47:21 +0530

    salary slip phase 1

--- src/components/common/PopupComponent.jsx
+++ src/components/common/PopupComponent.jsx
@@ -8,7 +8,7 @@ const PopupComponent = ({ heading = "Default Header", onClose }) => {
   const list = ["one","two"]
   return (
     <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-30">
-      <div className="homeBgColor rounded-lg gap-6 shadow-lg w-[600px] h-[600px] flex flex-col relative">
+      <div className="homeBgColor rounded-lg shadow-lg w-[600px] h-[600px] flex flex-col relative ">
         <div className="sticky top-0 w-full flex justify-between items-center px-6 pb-2 pt-6 z-20 border-b ">
           <h2 className="text-lg font-semibold custom-font-mavan-pro opacity-70 footer-font">
             {heading}
@@ -17,7 +17,8 @@ const PopupComponent = ({ heading = "Default Header", onClose }) => {
             &times;
           </button>
         </div>
-        <div className="flex justify-start pl-6 w-full gap-x-10">
+          <div className="px-6">
+          <div className="flex   w-full gap-x-10 py-3">
           {list.map((f) => (
             <NavLink
               to={f}
@@ -29,23 +30,21 @@ const PopupComponent = ({ heading = "Default Header", onClose }) => {
             </NavLink>
           ))}
         </div>
-        <div className="flex justify-center">
-          <div className="border-2 w-96 flex flex-col shadow h-52 border-dotted border-gray-600 rounded-lg justify-center items-center gap-y-4">
+        <div className="flex justify-center  py-5">
+          <div className="border-2 w-96 flex flex-col shadow h-52 border-dotted border-gray-400 rounded-lg justify-center items-center gap-y-6 ">
             <NewUpload />
-            <div className="flex justify-center p-2 items-center gap-x-2 ">
-              <h2 className="font-semibold custom-font-mavan-pro text-gray-500 text-xl">
-                Drag and Drop file or
-              </h2>
-              <BlueButton label="Browse" />
+            <div className="flex justify-center items-center gap-x-4 ">
+              <input type="file" text="" className="custom-font-mavan-pro homeUnitsFont py-2 font-semibold text-base px-8 rounded-lg "/>
             </div>
           </div>
         </div>
-        <div className="flex w-full px-14 h-26 items-center">
+        <div className="flex w-full h-26 items-center py-5">
           <AddComment />
         </div>
-        <div className="flex justify-end px-14">
+        <div className="flex justify-end ">
           <BlueButton label="Save" />
         </div>
+          </div>
       </div>
     </div>
   );
