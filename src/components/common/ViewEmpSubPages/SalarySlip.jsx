f594b2150760effbda892ad1a4325a591336f00c
izwrt <ishwart466@gmail.com>
2024-10-23 22:00:38 +0530

    months width causing issues

--- src/components/common/ViewEmpSubPages/SalarySlip.jsx
+++ src/components/common/ViewEmpSubPages/SalarySlip.jsx
@@ -1,20 +1,33 @@
 import React, { useState } from 'react';
 import apiFecthFiles from '../../../../api/apiFetchFiles';
-import PopupComponent from '../PopupComponent';
+import file from "../../../assets/images/file.png";
+import pdf from "../../../assets/images/pdf.png";
+import photo from "../../../assets/images/photo.png";
+import word from "../../../assets/images/word.png";
 import BlueButton from '../BlueButton';
+import PopupComponent from '../PopupComponent';
 
 
 ///salary slip datas are repeating
 
 const SalarySlip = () => {
 
+  const ImgConfig = {
+    pdf: pdf,
+    word: word,
+    docx: word,
+    png: photo,
+    jpeg: photo,
+    default: file
+  }
+
  const [isExiting, setIsExiting] = useState(false);
   const filesData = apiFecthFiles();
   
   const DescendingData = filesData.sort((a, b) => {
-    const dateA = new Date(a.date.split('/').reverse().join('-'));
-    const dateB = new Date(b.date.split('/').reverse().join('-'));
-    return dateB - dateA;
+    const dateA = a.monthId
+    const dateB = b.monthId
+    return dateA - dateB;
   });
 
   const onOpen = () => {
@@ -46,20 +59,29 @@ const SalarySlip = () => {
       <section className='flex flex-col gap-10 '>
           {DescendingData
         .filter((file, index, self) => 
-          index === self.findIndex(f => f.date === file.date)
+          index === self.findIndex(f => f.monthId === file.monthId)
         )
         .map(file => (
           <div className='custom-font-mavan-pro text-sm' key={file.id}>
-            <div className='flex w-full justify-center  items-center gap-2 font-semibold text-sm text-gray-700'>
-            <hr className='w-full border-t border-gray-400' />
-              {file.date}
-              <hr className='w-full border-t border-gray-400' />
+            <div className='flex w-full justify-center items-center gap-2 font-semibold text-sm py-4'>
+                <hr className='w-full border-t border-gray-400' />
+                <p className='text-black text-opacity-70 bg-red-400'>{file.month}</p>
+                <hr className='w-full border-t border-gray-400' />
             </div>
+            {
+            <div className='flex flex-wrap gap-20 px-4'>
             {DescendingData
               .filter(filteredFile => filteredFile.date === file.date)
               .map(filteredFile => 
-                <div key={filteredFile.id}>{filteredFile.fileName}</div>
+                <div className='flex flex-col gap-4 mt-10'>
+                  <div className='flex flex-col items-center w-20'>
+                    <img className="h-14 w-14" src={ImgConfig[filteredFile.fileName.split(".")[1]] || ImgConfig.default} alt={file.name} />
+                    <p className='text-sm max-w-full break-words overflow-hidden text-center'>{filteredFile.fileName}</p>
+                  </div>
+                </div>
               )}
+              </div>
+              }
           </div>
         ))}
       </section>
