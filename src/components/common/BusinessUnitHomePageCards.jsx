74b38262e2de8e73d1e3d250bc54cd728b3574f1
Nakul V Kamath <nakulkamath02@gmail.com>
2024-11-18 21:13:03 +0530

    Card issur resolved

--- src/components/common/BusinessUnitHomePageCards.jsx
+++ src/components/common/BusinessUnitHomePageCards.jsx
@@ -1,21 +1,23 @@
 function BusinessUnitHomePageCards({ unit }) {
+  const first = unit.name.split(" ", 1);
+  console.log(first);
   return (
     <div className=" w-[400px]">
       <div
         key={unit.id}
         className="bg-customBlue h-[200px] text-black rounded-3xl px-8 mx-4 pt-4 mb-4 drop-shadow-xl"
       >
-        <div className="flex gap-1">
-          <span className="custom-font-mavan-pro text-white text-2xl overflow-hidden whitespace-nowrap text-ellipsis">
-            {unit.name}
-          </span>
-        </div>
+        <span className="custom-font-mavan-pro text-white text-4xl overflow-hidden whitespace-nowrap text-ellipsis">
+          {first}
+        </span>
+        <br />
+        <span className="custom-font-mavan-pro text-white text-3xl">
+          Manager
+        </span>
 
-        <div className="flex justify-start w-full pt-4 pb-10">
-          <div className="text-xs custom-font-mavan-pro flex text-white font-semibold flex-col">
+        <div className="flex justify-start w-full pb-10">
+          <div className="text-xs custom-font-mavan-pro flex text-white opacity-70 font-semibold flex-col">
             <p className="text-base font-semibold pb-2">{unit.manager}</p>
-            <p className="text-sm pb-0.5">Department</p>
-            <p>{unit.description}</p>
           </div>
         </div>
       </div>
--- src/components/pages/BusinessUnit.jsx
+++ src/components/pages/BusinessUnit.jsx
@@ -192,5 +192,3 @@ export default function BusinessUnit() {
     </div>
   );
 }
-
-// {seetings}
