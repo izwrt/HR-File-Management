80d6f557b4900589b79d507fa8b284e44e1e70d3
izwrt <ishwart466@gmail.com>
2024-12-05 20:25:27 +0530

    task2

--- src/components/common/PopupComponent.jsx
+++ src/components/common/PopupComponent.jsx
@@ -4,7 +4,8 @@ import Close from "../../assets/images/close.png";
 import Cloud from "../../assets/images/cloud-file.gif";
 import NavContext from "../../utils/useContext/NavContext";
 
-//task 1 : File need to we uploaded to bacnked
+//task 1 : File need to we uploaded to bacnked.
+//task 2 : File should be stored in state.
 
 const PopupComponent = ({
   heading = "Default Header",
